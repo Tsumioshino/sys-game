5b94e20e040bb6aab9b1e11a537fe1721c4f36d4
Tsumioshino <marq938@gmail.com>
2023-10-07 21:53:24 -0300

    add: strong slash and applied effects

--- Player2.gd
+++ Player2.gd
@@ -22,26 +22,46 @@ var speed_scaling = 3
 var speed = 100 * speed_scaling
 
 onready var player = get_node("%Player")
-onready var btimer = get_node("/root/PlayerRoot/Player/Node/BuffTimer")
-onready var dtimer = get_node("/root/PlayerRoot/Player/Node/DebuffTimer")
+onready var btimer = get_node("%Player/Effect/BuffTimer")
+onready var dtimer = get_node("%Player/Effect/DebuffTimer")
 	
 onready var animation_tree = get_node("%AnimationTree")
 onready var animationState = animation_tree.get("parameters/playback")
 
 var isLocalPlayer = !is_network_master()
-
+var timeratk = Timer.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_tree.set_animation_player(player_class[class_count]) 
 	player.scale.x *= -1 if look_at == Direction.LEFT else 1
 	animation_tree.set("parameters/conditions/not_movement", true)
 	animation_tree.active = true
-	
+	add_child(timeratk)
+
+var consecutiveKeyPresses = 0
+var desiredKeyPresses = 2
+
 func _process(delta):
 	if Input.is_action_pressed("attack"):
+		if Input.is_action_just_pressed("attack"):
+			if timeratk.is_stopped():
+				animation_tree.set("parameters/conditions/consecutive_atk", false)
+				consecutiveKeyPresses = 0
+				timeratk.start(0.4)
+				timeratk.set_one_shot(true)
+			if (timeratk.get_time_left() > 0):
+				consecutiveKeyPresses += 1
+			else:
+				consecutiveKeyPresses = 0
 		animation_tree.set("parameters/conditions/attack", true)
+		if consecutiveKeyPresses >= desiredKeyPresses:
+			animation_tree.set("parameters/conditions/consecutive_atk", true)
+
 	else:
 		animation_tree.set("parameters/conditions/attack", false)
+		animation_tree.set("parameters/conditions/consecutive_atk", false)
+
+					
 		
 func _physics_process(delta):
 	if isLocalPlayer:
@@ -89,31 +109,43 @@ func take_damage(atk):
 
 func _on_AtaqueHit_body_entered(body):
 	print("hi at least")
-	if body.is_in_group("hurtbox"):
+	if body.is_in_group("hurtbox") and body != self:
 		print("where dmg")
 		body.take_damage(atk)    
+		
+func _on_AtaqueHit2_body_entered(body):
+	print("hi at least2")
+	if body.is_in_group("hurtbox") and body != self:
+		print("hey")
+		body.apply_effect_speed_negative(body)    
 		
 # Should be another Scene, probably
 # Effects shouldn't stack
 var has_speed_buff = false
 var has_speed_debuff = false
 
+onready var blinking = "%BLINK_EFFECT"
 func apply_effect_speed_positive(_player):
 	if not has_speed_buff:
 		speed_scaling += 2
 		has_speed_buff = true
 		btimer.start()
-		
+		get_node("%Tween").interpolate_property(get_node("%Player/SpritesAnimacao"), "self_modulate", Color(0xBBFF8CFF), Color(0xFFFFFFFF), btimer.get_wait_time())
+		get_node("%Tween").start()
+			
 func apply_effect_speed_negative(_player):
 	if not has_speed_debuff:
 		speed_scaling -= 2
 		has_speed_debuff = true
 		dtimer.start()
+		get_node("%Tween").interpolate_property(get_node("%Player/SpritesAnimacao"), "self_modulate", Color(0xFF7777FF), Color(0xFFFFFFFF), dtimer.get_wait_time())
+		get_node("%Tween").start()
 
 
 func _on_BuffTimer_timeout():
 	speed_scaling -= 2
 	has_speed_buff = false # Replace with function body.
+	#get_node("%Player/Sprite sAnimacao").self_modulate = Color(0xFFFFFFFF)
 
 func _on_DebuffTimer_timeout():
 	speed_scaling += 2
